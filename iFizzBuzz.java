d8091e846152a4ab736ecdec63838daa02591224
Derwin McGeary <derwinmcgeary+web@gmail.com>
2016-03-04 15:50:31 +0000

    proper error handling, now working

--- iFizzBuzz.java
+++ iFizzBuzz.java
@@ -3,12 +3,26 @@ import java.util.Scanner;
 public class iFizzBuzz {
     public static void main(String[] args){
 	System.out.println("Welcome to iFizzBuzz! Press ctrl-C to exit, or start doing integers.");
+	Scanner s = new Scanner(System.in);
+	int n;
 	
 	while(true) {
+	    
+	    try {
+		n = s.nextInt();
+	    }
+	    catch(Exception InputMismatchException) {
+		reject();
+		s.nextLine();
+		continue;
+	    }
 
-	    Scanner s = new Scanner(System.in);
-	    int n = s.nextInt();
-
+	    if(n <= 0) {
+		reject();
+		s.nextLine();
+		continue;
+	    }
+	    
 	    System.out.println(response(n));
 	}
     }
@@ -20,5 +34,10 @@ public class iFizzBuzz {
 	if(out.length() == 0) out += Integer.toString(n);
 	return out;
 	
+    }
+
+    public static void reject() {
+	    String rejected = new String("That's not a positive integer!");
+	    System.out.println(rejected);
     }
 }
