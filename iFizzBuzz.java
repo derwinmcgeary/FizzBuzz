9245e0f4d31504af2a41ec3a5a3d0d9eecc68302
Derwin McGeary <derwinmcgeary+web@gmail.com>
2016-03-04 11:57:31 +0000

    add exception handling and make iFizzBuzz more robust

--- iFizzBuzz.java
+++ iFizzBuzz.java
@@ -6,9 +6,25 @@ public class iFizzBuzz {
 	
 	while(true) {
 
+	    int n;
 	    Scanner s = new Scanner(System.in);
-	    int n = s.nextInt();
+	    try {
+	    n = s.nextInt();
+	    }
+	    catch(Exception InputMismatchException) {
+		reject();
+		continue;
+	    }
 
+	    if(n < 0) {
+		reject();
+		continue;
+	    }
+
+	    if(n==0) {
+		System.out.println("Nice try, good testing skills. Still not a positive integer");
+		continue;
+	    }
 	    System.out.println(response(n));
 	}
     }
@@ -20,5 +36,10 @@ public class iFizzBuzz {
 	if(out.length() == 0) out += Integer.toString(n);
 	return out;
 	
+    }
+
+    public static void reject() {
+	    String rejected = new String("That's not a positive integer!");
+	    System.out.println(rejected);
     }
 }
