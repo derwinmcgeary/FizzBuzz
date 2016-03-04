616f7a1b72bf617c3e5b6dc512445a353b69f6a8
Derwin McGeary <derwinmcgeary+web@gmail.com>
2016-03-04 09:57:41 +0000

    add interactive version

---
+++ iFizzBuzz.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class iFizzBuzz {
+    public static void main(String[] args){
+	System.out.println("Welcome to iFizzBuzz! Press ctrl-C to exit, or start doing integers.");
+	while(true) {
+	Scanner s = new Scanner(System.in);
+	int n = s.nextInt();
+
+	System.out.println(response(n));
+	}
+    }
+
+    public static String response(int n) {
+	String out = new String("");
+	if(n%3==0) out += "Fizz";
+	if(n%5==0) out += "Buzz";
+	if(out.length() == 0) out += Integer.toString(n);
+	return out;
+
+    }
+}
