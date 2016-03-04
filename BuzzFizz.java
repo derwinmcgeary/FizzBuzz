4b846bb43ff26fbd7a60b30dcbcf1ceb478f445f
Derwin McGeary <derwinmcgeary+web@gmail.com>
2016-03-04 09:41:46 +0000

    Added BuzzFizz, a nicer way of doing the same thing

---
+++ BuzzFizz.java
@@ -0,0 +1,12 @@
+public class BuzzFizz {
+    public static void main(String[] args){
+	for(int i=1; i < 100; i++) {
+	    String out = new String("");
+	    if(i%3==0) out += "Fizz";
+	    if(i%5==0) out += "Buzz";
+	    if(out.length() == 0) out += Integer.toString(i);
+	    System.out.println(out);
+	}
+	       
+    }
+}
